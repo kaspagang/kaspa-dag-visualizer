6f38f2f0382defa57f2424af4c7a9e73f2f9fd59
Tomer Levy <tmrlvi@gmail.com>
2022-01-22 10:30:36 +0200

    Added NavBar with controls + crude hierarchical layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import React, { ReactNode, useEffect, useState } from "react";
-import { SigmaContainer, useSigma, useLoadGraph, SearchControl, ControlsContainer, ForceAtlasControl} from "react-sigma-v2";
-import "react-sigma-v2/lib/react-sigma-v2.css";
+import { SigmaContainer, useSigma, useLoadGraph, SearchControl, ControlsContainer, ForceAtlasControl, useRegisterEvents} from "react-sigma-v2";
 import Graph from "graphology";
 import circularLayout from "graphology-layout/circular";
 import Graphml from "graphology-graphml";
 import Sigma from "sigma";
 
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Spinner from 'react-bootstrap/Spinner';
+import Modal from 'react-bootstrap/Modal';
+
+import "react-sigma-v2/lib/react-sigma-v2.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import hierarchialLayout from "./hierarchical";
+
+import glob from "glob";
+
 //import erdosRenyi from "graphology-generators/random/erdos-renyi";
 
-export const TestGraph = () => {
+/*const Loading = () => {
+  const [show, setShow] = useState("sm-down");
+  const registerEvents = useRegisterEvents();
+
+  registerEvents({
+    // @ts-ignore
+    cameraUpdated: event => setShow(false),
+  });
+  // onHide={() => setShow(false)}
+
+  // @ts-ignore
+  return <Modal show={show} fullscreen={true}>
+    <Spinner animation="border" />
+  </Modal>;
+}*/
+
+export const TestGraph = (options) => {
   const loadGraph = useLoadGraph();
-  fetch("./data/network_start.graphml")
+  fetch(options.path)
     .then((res) => res.text())
     .then((gml) => {
       // Parse GEXF string:
@@ -20,8 +48,10 @@ export const TestGraph = () => {
         attrs["label"] = node;
         return attrs;
       });
-      hierarchialLayout(graph, {});
+      console.log(options);
+      hierarchialLayout(graph, {"height": options.height});
       loadGraph(graph);
+      console.log("Done all!")
     });
 
   return null;
@@ -29,10 +59,38 @@ export const TestGraph = () => {
 
 export default function App() {
 
+  const [height, setHeight] = useState("blueScore");
+  console.log(height);
 
+  /*glob("./data/*.graphml", null, function (er, files) {
+    alert("hi");
+    console.log("hi");
+    console.log(files)
+  })*/
+
+  //
   return <div>
+  <Navbar variant="dark" bg="dark" expand="lg">
+    <Container fluid>
+      <Navbar.Brand href="#home">Kaspa DAG Explorer</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-dark" />
+      <Navbar.Collapse id="navbar-dark">
+        <Nav>
+          <NavDropdown
+            id="nav-dropdown-dark"
+            title="Height By"
+            menuVariant="dark"
+          >
+            <NavDropdown.Item onClick={() => setHeight("blueScore")}>Blue Score</NavDropdown.Item>
+            <NavDropdown.Item onClick={() => setHeight("daaScore")}>DAA Score</NavDropdown.Item>
+            <NavDropdown.Item onClick={() => setHeight("timeInSeconds")}>Timestamp</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   <SigmaContainer style={{ height: "800px", width: "1900px" }}>
-  <TestGraph/>
+  <TestGraph height={height} path={"./data/network_start.graphml"}  />
   <ControlsContainer position={"top-right"}>
     <SearchControl />
     <ForceAtlasControl/>
--- src/hierarchical.ts
+++ src/hierarchical.ts
@@ -3,9 +3,47 @@ import isGraph from 'graphology-utils/is-graph';
 
 var DEFAULTS = {
   "height": "blueScore",
-  "yscale": 10.,
-  "xscale": 100.,
+  "yscale": 1000.,
+  "xscale": 1.,
+  "orderLayers": false
 };
+
+function* permute(permutation) {
+  var length = permutation.length,
+      c = Array(length).fill(0),
+      i = 1, k, p;
+
+  yield permutation.slice();
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      yield permutation.slice();
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+}
+
+
+function numCrossings(graph, nodes){
+  let crossings = 0;
+  for(let i=0; i<nodes.length; i++){
+    for(let j=0; j<nodes.length; j++){
+      for (let v of graph.inNeighbors(nodes[i])){
+        for (let u of graph.inNeighbors(nodes[j])){
+          crossings +=  Number(v["x"] > u["x"]);
+        }
+      }
+    }
+  }
+  return crossings;
+}
 
 export default function hierarchialLayout(graph, options) {
   if (!isGraph(graph))
@@ -16,19 +54,62 @@ export default function hierarchialLayout(graph, options) {
   options = resolveDefaults(options, DEFAULTS);
 
   const acc = graph.reduceNodes((acc, node, attrs) => {
-    if (acc[0] === null || attrs[options.height] < acc[0]) {
-      acc[0] = attrs[options.height];
+    // Add node to level
+    if (attrs[options.height] !== undefined || attrs[options.height] !== null){
+      if (!(attrs[options.height] in acc["levels"])) {
+        acc["levels"][attrs[options.height]] = []
+      }
+      acc["levels"][attrs[options.height]].push(node)
     }
-    if (!(attrs[options.height] in acc[1])) {
-      acc[1][attrs[options.height]] = 0;
+    // Get Minimum
+    if (acc["min"] === null || attrs[options.height] < acc["min"]) {
+      acc["min"] = attrs[options.height];
     }
-    acc[1][attrs[options.height]] = acc[1][attrs[options.height]] + 1;
+    // Get Maximum
+    if (acc["max"] === null || attrs[options.height] > acc["max"]) {
+      acc["max"] = attrs[options.height];
+    }
+
+    // Get width
+    if (!(attrs[options.height] in acc["widths"])) {
+      acc["widths"][attrs[options.height]] = 0;
+    }
+    acc["widths"][attrs[options.height]] = acc["widths"][attrs[options.height]] + 1;
     return acc;
-  }, [null, {}])
+  }, { "min": null, "max": null, "widths": {}, "levels": {}})
+
+  if (options.orderLayers) {
+    // @ts-ignore
+    let hadConflicts = true;
+    while (hadConflicts) {
+      console.log(hadConflicts);
+      hadConflicts = false;
+      acc["levels"] = Object.fromEntries(
+          Object.entries(acc["levels"]).map(([k, level]) => {
+            let bestProposal = level;
+            let bestCost = numCrossings(graph, bestProposal);
+            for (let proposal of permute(level)) {
+              let cost = numCrossings(graph, proposal);
+                if (cost < bestCost) {
+                hadConflicts = true;
+                bestCost = cost;
+                bestProposal = level;
+              }
+            }
+            return [k, bestProposal];
+          })
+      );
+      console.log("Done!");
+    }
+  }
 
   graph.updateEachNodeAttributes( (node, attrs) => {
-    attrs["x"] = (acc[1][attrs[options.height]]--)*options.xscale;
-    attrs["y"] = (attrs[options.height] - acc[0])*options.yscale;
+    attrs["y"] = (attrs[options.height] - acc["min"])/(acc["max"] - acc["min"])*options.yscale;
+
+    let length = acc["levels"][attrs[options.height]].length;
+    let idx = acc["levels"][attrs[options.height]].indexOf(node);
+    attrs["x"] = (idx - (length+1)/2)*options.xscale;
+
     return attrs;
   });
 }
