dab9f908dade07df5db1c37185d9995774797636
Tomer Levy <tmrlvi@gmail.com>
2022-01-22 13:55:06 +0200

    Added graph list from `./data`

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import hierarchialLayout from "./hierarchical";
 
+import fs from "fs";
+import path from "path";
+
 import glob from "glob";
 
 //import erdosRenyi from "graphology-generators/random/erdos-renyi";
@@ -39,6 +42,15 @@ import glob from "glob";
 
 export const TestGraph = (options) => {
   const loadGraph = useLoadGraph();
+  if (options.path === null) {
+    const sigma = useSigma();
+    const graph = sigma.getGraph();
+    graph.addNode("Jessica", { label: "Jessica", x: 1, y: 1, color: "#FF0", size: 10 });
+    graph.addNode("Truman", { label: "Truman", x: 0, y: 0, color: "#00F", size: 5 });
+    graph.addEdge("Jessica", "Truman", { color: "#CCC", size: 1 });
+    return null;
+  }
+  console.log("Loading File: ", options.path);
   fetch(options.path)
     .then((res) => res.text())
     .then((gml) => {
@@ -58,15 +70,17 @@ export const TestGraph = (options) => {
 }
 
 export default function App() {
-
   const [height, setHeight] = useState("blueScore");
+  const [graphFile, setGraphFile] = useState(null);
   console.log(height);
 
-  /*glob("./data/*.graphml", null, function (er, files) {
-    alert("hi");
-    console.log("hi");
-    console.log(files)
-  })*/
+  const filesArray = fs.readdirSync(path.join(__dirname, 'data'))
+                       .filter((name) => name.endsWith("graphml"));
+  const files = filesArray.map(
+      (name, idx) =>
+          <NavDropdown.Item onClick={() => setGraphFile("./data/" + name)} key={idx}>{name}</NavDropdown.Item>
+  );
+  console.log(files);
 
   //
   return <div>
@@ -76,24 +90,24 @@ export default function App() {
       <Navbar.Toggle aria-controls="navbar-dark" />
       <Navbar.Collapse id="navbar-dark">
         <Nav>
-          <NavDropdown
-            id="nav-dropdown-dark"
-            title="Height By"
-            menuVariant="dark"
+          <NavDropdown id="nav-dropdown-dark" title="Height By" menuVariant="dark"
           >
             <NavDropdown.Item onClick={() => setHeight("blueScore")}>Blue Score</NavDropdown.Item>
             <NavDropdown.Item onClick={() => setHeight("daaScore")}>DAA Score</NavDropdown.Item>
             <NavDropdown.Item onClick={() => setHeight("timeInSeconds")}>Timestamp</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown id="nav-dropdown-dark" title="Graph File"  menuVariant="dark">
+            {files}
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
   <SigmaContainer style={{ height: "800px", width: "1900px" }}>
-  <TestGraph height={height} path={"./data/network_start.graphml"}  />
-  <ControlsContainer position={"top-right"}>
+  <TestGraph height={height} path={graphFile}  />
+  <ControlsContainer position={"top-left"}>
     <SearchControl />
-    <ForceAtlasControl/>
+    {/*<ForceAtlasControl/>*/}
   </ControlsContainer>
   </SigmaContainer>
   </div>;
